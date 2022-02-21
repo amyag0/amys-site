2877c66d2610a850c63df63ef03a36745e3b83dc
amyag0 <amyago19@gmail.com>
2022-02-21 16:33:47 -0500

    add index.html, add index.js, add style.css

---
+++ index.js
@@ -0,0 +1,45 @@
+function changeBackground(){
+    document.getElementById(heading-one).style.color="blue"
+}
+function backToNormal(){
+    document.getElementById(heading-one).style.color="yellow"
+}
+
+
+function changeBackground(){
+    document.getElementById(heading-two).style.color="blue"
+}
+function backToNormal(){
+    document.getElementById(heading-two).style.color="yellow"
+}
+
+
+function changeBackground(){
+    document.getElementById(heading-three).style.color="blue"
+}
+function backToNormal(){
+    document.getElementById(heading-three).style.color="yellow"
+}
+
+
+function changeBackground(){
+    document.getElementById(heading-four).style.color="blue"
+}
+function backToNormal(){
+    document.getElementById(heading-four).style.color="yellow"
+}
+
+
+function changeBackground(){
+    document.getElementById(heading-five).style.color="blue"
+}
+function backToNormal(){
+    document.getElementById(heading-five).style.color="yellow"
+}
+
+function changeBackground(){
+    document.getElementById(footer-heading).style.color="blue"
+}
+function backToNormal(){
+    document.getElementById(footer-heading).style.color="yellow"
+}
