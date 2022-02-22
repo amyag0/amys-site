345c91e4028964c02ebb2ae9f5d492e4ebba39ee
amyag0 <amyago19@gmail.com>
2022-02-21 20:39:34 -0500

    add index.html, add index.js, add style.css

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-function changeBackground(){
+function beforeChair(){
+    document.getElementById("reup").src="https://blogger.googleusercontent.com/img/a/AVvXsEi_nUkEkHiC9ldSrFCWdEoaByKm48jViH87eYX7NbGVz9Z4y52rVNV1u82rTtJbfK5EyiN2Ol3_yuoM4aXZJaLYX7_hK-HdByRNkJtv1mdTdqcT5OTVqr-0XLiiyDNO5OZEBE9fRzshUbHikjcaxusgE0eZO3YjoesK4po76EnYuLVrmkX_75RXh-OM1g=s320"
+}
+function afterChair(){
+    document.getElementById("reup").src="https://blogger.googleusercontent.com/img/a/AVvXsEgjkCd6varPbXLYm4qJokYs7Yz0wsOYe7bzSon2e1oCqMSg1adRp37ofsYSRa2dOQ6v5gWLAGAw6NtZ5LCxekiNEQ_onvViVVyKaPGCdOE1qkPU8InkLRRjv808aLO7UlQ2AC1QfB7yRFY0zvr2qJMPgGeaGOWosEbu4_o70NHBpx2tgLnEoM5nKTHHaQ=s320"
+}
+
+
+
+function beforeMirror(){
+    document.getElementById("finish").src="https://blogger.googleusercontent.com/img/a/AVvXsEhgios_wDKD8LLBz3Dc0i1WcbFt2XTL5FBjYKF7W3AURJprh2g55axF53NeYt7YDmFWFMQDL5IuOXN9OIrgvhPDFrI8ZxSDMp-mS8xXhC6uo6dNg_e1dOuCANL2RxFavGDHDjD8rkVlbWzsoB5LFYsPsAOL4hz4zZXD_Ey92XffgQcRYT23zkg-XCW6fg=s320"
+}
+function afterMirror(){
+    document.getElementById("finish").src="https://blogger.googleusercontent.com/img/a/AVvXsEhB8k4DEqQcVGXm_csipPao7i8aa_pqJ8Hwyz7jcKNIL_jMY6WW0uFL3c_YSRCtFIuaKyMXv785SKqXFLzOo6He2tn3YGvFzZLqYUviWPRACeB9fpL5qTiFbqyi2LDajV4WeK5IamP1swCIh5Fb0yqb7wQ04nv5RSUBti1gMbnLvUMAXUsp1vG4KOA1Yg=s320"
+}
+
+
+
+function beforeBuild(){
+    document.getElementById("build").src="https://blogger.googleusercontent.com/img/a/AVvXsEg81aoAk2E992tC27uNf5k5XeWam-JkP8oRDJRYSu1vPis8u3fQlRGsE4w6d5qOdEjuZlCojclbunimDbwqVavPyvu7R2UtKNX9VOS0l4kj4vssj4NS-KFyeuofTl8wHg1piWiP6CZz3x28d8xV1QCnfUj_r6aAQJTXO2chhVWJ0LJj3pMlT2QBrm0s5Q=s320"
+}
+function afterBuild(){
+    document.getElementById("build").src="https://blogger.googleusercontent.com/img/a/AVvXsEgDj8yYalEirJZSpCjUV5goSmvdtJUuzAxdyQkT-IbfIQs3ufcD8puuPNEez7_HqnLni9ZJGvirGF1ks73w6dio9lq-Th96yazU6Lb-H1EI7BdhZ29LEbF-zdO77WSLL1utxXVx9EtvIGOpEEk9m9dCIcnmBTGI6VR_4kU4zm4FpGc2_fiUCzl6v1HeXg=s320"
+}
+
+
+
+function beforeGlass(){
+    document.getElementById("glass").src="https://blogger.googleusercontent.com/img/a/AVvXsEjDuBDgC5Gz4XWPP5mcTs8kzqEDFsh9brZ3AHO3XUlJj6axGvG3ICAPhQcczA290quYDzQ2LFej_UdCvBdtnwFZygRdL31z4iF1Pfgzv8eXPDSSFx7YFmnjbbsSsgbZ3_h0DbnG1_bYYAvBneSwjDJw7ZBL8plJDZyAg6L5Kflx3PRvMoqEzZuncfWl9A=s320"
+}
+function afterGlass(){
+    document.getElementById("glass").src="https://blogger.googleusercontent.com/img/a/AVvXsEi2Jc08KN7JEluakqeQL6tnF1Hy5fMW_5w0Rk5cQFcH0wpHlIpwckiOGau3F_gLygeG4Nx1mkT0SzkVlHeEwlqkl1AV1cyXcmM-gyNKpQD9nXXINHSahwLffmKdjrdGe5fe6D0aWweSD-hGFwsGm8wP41eMQGVOx6HEDbx7z2PDZqhyqg1jZHyreqKB_g=s320"
+}
+
+
+
+/*function changeBackground(){
     document.getElementById(heading-one).style.color="blue"
 }
 function backToNormal(){
@@ -42,4 +78,4 @@ function changeBackground(){
 }
 function backToNormal(){
     document.getElementById(footer-heading).style.color="yellow"
-}
+}*/
